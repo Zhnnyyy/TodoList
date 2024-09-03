9a97389dc43cb028e209ac748721f78014080148
Zhnnyyy <perpetuaedemmanuel1225@gmail.com>
2024-09-03 13:03:56 +0800

    INITIAL

--- app/ConvexClientProvider.js
+++ app/ConvexClientProvider.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { SignIn, SignUp, useAuth } from "@clerk/clerk-react";
+
 import {
   Authenticated,
   AuthLoading,
@@ -10,30 +11,25 @@ import {
 } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { quantum } from "ldrs";
-import { redirect } from "next/navigation";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 quantum.register();
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
-
 export function ConvexClientProvider({ children }) {
+  const path = usePathname();
+  const [isSignIn, setIsSignIn] = useState(false);
+  useEffect(() => {
+    setIsSignIn(path.match("/sign-in"));
+  }, [path]);
   return (
     <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-      {/* <Unauthenticated>
+      <Unauthenticated>
         <div className="h-[100vh] flex justify-center items-center">
-          {redirect(
-            (
-              <>
-                <SignIn />
-              </>
-            ) || (
-              <>
-                <SignUp />
-              </>
-            )
-          )}
+          {isSignIn && <SignIn />}
+          {!isSignIn && <SignUp />}
         </div>
-      </Unauthenticated> */}
-      {/* <Authenticated>{children}</Authenticated> */}
-      {children}
+      </Unauthenticated>
+      <Authenticated>{children}</Authenticated>
       <AuthLoading>
         <div className="h-[100vh] flex justify-center items-center">
           <l-quantum size="150" speed="1.75" color="black"></l-quantum>
